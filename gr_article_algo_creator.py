bafe165ebd9b6916381fa31f19bdef410c8f2799
Chris Drymon <chrisdrymon@yahoo.com>
2020-10-29 18:43:53 -0500

    Began making the algorithm maker.

---
+++ gr_article_algo_creator.py
@@ -0,0 +1,55 @@
+import os
+from bs4 import BeautifulSoup
+from collections import Counter
+
+pos0_dict = {'a': 'adj', 'n': 'noun', 'v': 'verb', 'd': 'adv', 'c': 'conj', 'g': 'conj', 'r': 'adposition', 'b': 'conj',
+             'p': 'pronoun', 'l': 'article', 'i': 'interjection', 'x': 'other', 'm': 'numeral', 'e': 'interjection'}
+pos2_dict = {'s': 'singular', 'p': 'plural', 'd': 'dual'}
+pos4_dict = {'i': 'indicative', 's': 'subjunctive', 'n': 'infinitive', 'm': 'imperative', 'p': 'participle',
+             'o': 'optative'}
+proiel_pos_dict = {'A': 'adj', 'D': 'adv', 'S': 'article', 'M': 'numeral', 'N': 'noun', 'C': 'conj', 'G': 'conj',
+                   'P': 'pronoun', 'I': 'interjection', 'R': 'adposition', 'V': 'verb'}
+original_folder = os.getcwd()
+folder_path = os.path.join(os.environ['HOME'], 'Google Drive', 'Greek Texts', 'Annotated')
+indir = os.listdir(os.path.join('data', 'corpora', 'greek', 'annotated'))
+file_count = 0
+
+
+# This returns the part-of-speech or the mood if the part-of-speech is a verb for a given word.
+def poser(f_word):
+    if f_word.has_attr('postag'):
+        if len(f_word['postag']) > 0:
+            pos0 = f_word['postag'][0]
+            if pos0 in pos0_dict:
+                f_pos = pos0_dict[pos0]
+            else:
+                f_pos = 'other'
+        else:
+            f_pos = 'other'
+    elif f_word.has_attr('part-of-speech'):
+        if len(f_word['part-of-speech']) > 0:
+            pos0 = f_word['part-of-speech'][0]
+            if pos0 in proiel_pos_dict:
+                f_pos = proiel_pos_dict[pos0]
+            else:
+                f_pos = 'other'
+        else:
+            f_pos = 'other'
+    else:
+        f_pos = 'other'
+    return f_pos
+
+
+for file in indir:
+    if file[-4:] == '.xml':
+        file_count += 1
+        print(file_count, file)
+        xml_file = open(file, 'r')
+        soup = BeautifulSoup(xml_file, 'xml')
+        sentences = soup.find_all('sentence')
+        for sentence in sentences:
+            tokens = sentence.find_all(['word', 'token'])
+            for token in tokens:
+                if token['lemma'] == 'ὁ' and poser(token) == 'article':
+                    pass
+
